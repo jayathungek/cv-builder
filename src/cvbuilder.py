39b89992cd98bdd4b219c387783b739da73f5110
jayathungek <jayathunge.work@gmail.com>
2019-10-10 22:46:35 +0400

    add existing code to new repo

---
+++ src/cvbuilder.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from tkinter import * 
+import os
+
+def compile_latex():
+    print("Compiling...")
+    os.system("pdflatex ../latex/cv.tex")
+    os.system("mv *.aux *.log *.pdf ../latex")
+    print("Done.")
+
+
+win = Tk()
+
+compile_button = Button(win, text="Compile")
+compile_button.pack(side=LEFT, padx=10, pady=10)
+compile_button.configure(command=compile_latex)
+
+win.mainloop()
